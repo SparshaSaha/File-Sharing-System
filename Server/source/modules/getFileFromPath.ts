34da8a3c3369f4aaec71cd3cb6b87d9bccb52e8c
Sparsha Saha <saha.sparsha@gmail.com>
2020-12-07 00:12:29 +0530

    server updated

--- Server/source/modules/getFileFromPath.ts
+++ Server/source/modules/getFileFromPath.ts
@@ -1,12 +1,10 @@
 import { errorCodes } from "../errorCodes/errorCodes";
 import { IApp, IRequest, IResponse } from "../interfaces/express.interface";
-import { accessSync } from 'fs';
 
 export const getFileFromPath = (app: IApp) => {
     app.get("/getFileFromPath", (request: IRequest, response: IResponse) => {
         const filePath = request.query.filePath as string;
         try {
-            accessSync(filePath);
             response.download(filePath);
         } catch(error) {
             response.status(errorCodes.FILE_NOT_FOUND_ERROR_CODE).send(errorCodes.FILE_NOT_FOUND_ERROR);
