cfc5f2bbd649afbda6589200e9a0a21af8c4a651
Sparsha Saha <saha.sparsha@gmail.com>
2020-12-25 19:54:01 +0530

    updated list example

--- Client/sources/components/app.tsx
+++ Client/sources/components/app.tsx
@@ -4,25 +4,29 @@ import { FileUploader } from "./fileUpload/fileUploader";
 export interface IAppProps {}
 
 export default function IApp(props: IAppProps) {
-  const [fileToUpload, setFileToUpload] = React.useState(undefined);
+  // Callback to execute when clicked on directory
+  const directoryC = React.useCallback((event: React.SyntheticEvent) => {
+    console.log(`Clicken on Dir:- ${(event.target as HTMLElement).innerHTML}`);
+  }, []);
 
-  const onFileUpload = React.useCallback(() => {
-    console.log("Upload File");
+  // Callback to execute when clicked on a file (download the file)
+  const fileC = React.useCallback((event: React.SyntheticEvent) => {
+    console.log(`Clicken on File:- ${(event.target as HTMLElement).innerHTML}`);
   }, []);
   return (
     <>
       <FileList
         headerNames={["File Name", "File Size", "File Path"]}
         data={[
-          ["Sparsha.txt", "200kb", "TestPath"],
-          ["Spurjya.txt", "200kb", "TestPath"],
-          ["Sparsha.txt", "200kb", "TestPath"],
-          ["Spurjya.txt", "200kb", "TestPath"],
-          ["Sparsha.txt", "200kb", "TestPath"],
-          ["Spurjya.txt", "200kb", "TestPath"],
-          ["Sparsha.txt", "200kb", "TestPath"],
-          ["Spurjya.txt", "200kb", "TestPath"],
+          ["Sparsha.txt", "200kb", "TestPath", "dir"],
+          ["Spurjya.txt", "200kb", "TestPath", "file"],
+          ["Sparsha.pdf", "200kb", "TestPath", "dir"],
+          ["Spurjya.pdf", "200kb", "TestPath", "file"],
+          ["Sparsha.pptx", "200kb", "TestPath", "dir"],
+          ["Spurjya.pptx", "200kb", "TestPath", "file"],
         ]}
+        directoryOnClick={directoryC}
+        fileOnClick={fileC}
       />
       <FileUploader />
     </>
