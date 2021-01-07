788dc9fdc7cc13142df58d07cd08ec20de010c78
Sparsha Saha <saha.sparsha@gmail.com>
2021-01-08 01:53:58 +0530

    added reload for client on code change

--- Client/webpack.development.config.js
+++ Client/webpack.development.config.js
@@ -9,6 +9,8 @@ const htmlPlugin = new HtmlWebPackPlugin({
 module.exports = {
     mode: "development",
     entry: "./sources/index.tsx",
+    watch: true,
+    devtool: "inline-source-map",
     output: {
         path: path.resolve(__dirname, "dev"),
         filename: "main.js"
@@ -17,10 +19,17 @@ module.exports = {
         // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
-
+    devServer: {
+        contentBase: './dev'
+    },
+    watchOptions: {
+        aggregateTimeout: 1000,
+        poll: 1000,
+        ignored: /node_modules/
+    },
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
             { test: /\.tsx?$/, loader: "ts-loader" }
         ]
     },
